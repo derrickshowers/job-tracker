e9884796d6ee5af92bf14883329369625940506d
Derrick Showers <dshowers@linkedin.com>
2014-11-16 01:23:08 -0800

    index page, dashboard and some view logic to go along with it

--- jobtracker/urls.py
+++ jobtracker/urls.py
@@ -6,6 +6,10 @@ urlpatterns = patterns('',
     # url(r'^$', 'jobtracker.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', 'jobtracker.views.index'),
+    url(r'^dashboard/', 'jobtracker.views.dashboard'),
+
+
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', include('jobs.urls', namespace='jobs')),
+    url(r'^jobs/', include('jobs.urls', namespace='jobs')),
 )
---
+++ jobtracker/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from jobs.models import Job
+
+# Create your views here.
+def index(request):
+	return render(request, 'common/index.html')
+
+def dashboard(request):
+    jobs_list = Job.objects.all()
+    context = { 'jobs_list': jobs_list }
+    return render(request, 'jobs/index.html', context)
