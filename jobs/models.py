10f5feab0bd24c3d5ad5a6aefef13176ac206f37
Derrick Showers <dshowers@linkedin.com>
2014-11-16 21:15:44 -0800

    adding user to jobs

--- jobs/models.py
+++ jobs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Job(models.Model):
@@ -7,6 +8,7 @@ class Job(models.Model):
     description = models.TextField(max_length=1000)
     url = models.URLField(max_length=200)
     status = models.CharField(max_length=200)
+    user = models.ForeignKey(User)
 
     def __str__(self):
         return self.title + ' - ' + self.company
