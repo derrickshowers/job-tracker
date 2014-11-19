f4cc003f21b64b4f3f233ae046e6b19626b46587
sarahlshowers <sarahlshowers@gmail.com>
2014-11-18 20:35:42 -0800

    validations working on submit

--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,26 @@
-document.ready
-    console.log("hello world");
+console.log('hello');
 
+$("#newJobForm").submit(function(e){
+    console.log(e);
+    // e.preventDefault();
+
+    var isValid = validateForm(this);
+    return isValid;
+});
 
 function validateForm(form) {
-    if(form.title.value == ""){
+    console.log(form);
+    if(form.title.value === ""){
         alert("Error: Input is empty");
-        form.title.focus();
+        // form.title.focus();
         console.log("no input");
         return false;
     }
-    if(form.company.value == ""){
+    if(form.company.value === ""){
         alert("Error: Input is empty");
-        form.company.focus();
+        // form.company.focus();
         return false;
     }
     return true;
 }
+
